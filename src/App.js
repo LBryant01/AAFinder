85bde301441f3c2f16867740674f80b1975a84b8
LBryant01 <london.bryant.lb@gmail.com>
2026-05-28 11:04:37 -0600

    Update placeholder text for unit input field

--- src/App.js
+++ src/App.js
@@ -177,7 +177,7 @@ export default function App() {
             <input
               className="unit-input"
               type="text"
-              placeholder="e.g. 21st Space Wing, 1st Space Operations Squadron..."
+              placeholder="e.g. 319th Combat Training Squadron, 1st Space Operations Squadron..."
               value={unit}
               onChange={(e) => {
                 setUnit(e.target.value);
